0ee1dd61a36fd37aa65e9ea3b5b093d82356ee7c
Nancy504 <nanyp2002@gmail.com>
2025-04-29 22:51:15 -0400

    Tryig to get this to render
    Trying

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.post('/donate', (req, res) => {
+  const { name, email, cause } = req.body;
+  console.log(`Pledge received from ${name} (${email}) for ${cause}`);
+
+  res.json({
+    message: `Thanks, ${name}! Your pledge to the ${cause} resource has been received.`,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
